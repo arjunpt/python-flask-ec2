ed1664f390f7fd9fcfcc8a10fd92d79730672808
arjunpt <arjunpt2006@gmail.com>
2024-08-13 16:23:13 +0530

    add

--- test/test.py
+++ test/test.py
@@ -1,11 +1,5 @@
-from src.app import app
+from app import index
+
 
 def test_index():
-    # Create a test client using the Flask application configured for testing
-    with app.test_client() as client:
-        # Send a GET request to the index route
-        response = client.get("/")
-        # Check that the response is "Hello, world!"
-        assert response.data == b"Hello, world!"
-        # Check that the status code is 200
-        assert response.status_code == 200
+    assert index() == "Hello, world!"
